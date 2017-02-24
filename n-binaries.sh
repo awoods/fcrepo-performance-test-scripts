b00f36d7818daa62867e56cf2db663f6f4a81136
Ubuntu <ubuntu@ip-10-149-161-101.ec2.internal>
2017-02-24 23:04:48 +0000

    Adds file size param and ensures that all binaries are unieque.

--- n-binaries.sh
+++ n-binaries.sh
@@ -5,12 +5,23 @@ BASE=http://localhost:8080/rest/$RANDOM
 # create a collection
 curl -X PUT $BASE  && echo
 
+
+FILE_SIZE_IN_KB=$2
+
+BINARY_FILE=binary
+dd if=/dev/zero of=$BINARY_FILE bs=1000 count=$FILE_SIZE_IN_KB
+
 BEGIN=$(date +%s)
 N=0
 MAX=$1
 while [ $N -lt $MAX ]; do
   # add a binary
-  curl -i -X POST --data-binary "@picture-128KB.jpg" $BASE
+  # change the binary file temptlate
+  newBinary=${BINARY_FILE}_${N}.dat
+  cp $BINARY_FILE $newBinary
+  echo $N >> $newBinary
+  curl -i -X POST --data-binary "@$newBinary" $BASE
+  rm $newBinary
 
   N=$(( $N + 1 ))
 
@@ -19,6 +30,7 @@ while [ $N -lt $MAX ]; do
   fi
 done
 
+rm $BINARY_FILE
 
 NOW=$(date +%s)
 DIFF=$(($NOW - $BEGIN))
@@ -27,5 +39,5 @@ SECS=$(($DIFF % 60))
 HOURS=$(($DIFF / 3600))
 DAYS=$(($DIFF / 86400))
 echo \n
-echo "Test: Upload $MAX 128K binary files"
+echo "Test: Upload $MAX unique $FILE_SIZE KB  binary files"
 printf "\rTotal Elapsed Time: %3d Days, %02d:%02d:%02d" $DAYS $HOURS $MINS $SECS
